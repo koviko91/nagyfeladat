3dca08577e8612ed96c49f10f9a2be8d6572f4c2
Kovats Oliver <koviko@gmail.com>
2018-04-15 12:44:56 +0200

    feat:components ,service, installed pug, set webpack.config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { HttpClient } from 'selenium-webdriver/http';
+import { checkAndUpdateElementDynamic } from '@angular/core/src/view/element';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  datas: any;
+
+  constructor(public http: Http) {
+    this.getAll();
+  }
+
+  getAll() {
+    this.http.get('http://localhost:3000/user').subscribe(
+      data => {
+        console.log(data);
+      });
+  }
+  /* create() {
+    this.http.post('http://localhost:3000/user/', this.adat).subscribe(
+      data => {
+        this.errorHandling(data);
+      });
+  }
+
+  update() {
+    this.http.put(`http://localhost:3000/user/${this.modal['id']}`, this.modal)
+      .subscribe(data => {
+        this.errorHandling(data);
+      });
+  }
+
+  deleteRow(id) {
+    this.http.delete(`http://localhost:3000/user/${id}`)
+      .subscribe(data => {
+        this.errorHandling(data);
+      });
+  }
+
+  modalChange(id) {
+    let choosen = this.datas.filter(item => item.id == id)[0];
+    this.modal = Object.assign({}, choosen);
+  } */
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpreqService } from './httpreq.service';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
+import { ApiComponent } from './api/api.component';
+import { ConnectComponent } from './connect/connect.component';
 
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { component: HomeComponent, path: 'home' },
+  { component: ProfileComponent, path: 'profile' },
+  { component: KnowledgeComponent, path: 'knowledge' },
+  { component: ApiComponent, path: 'api' },
+  { component: ConnectComponent, path: 'connect' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    KnowledgeComponent,
+    ApiComponent,
+    ConnectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [HttpreqService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
